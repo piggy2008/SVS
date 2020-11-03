2838c7479dc2adaabdb934d362654abcdd074385
piggy2008 <810656271@qq.com>
2020-11-03 16:24:41 +0800

    add evaluation code

---
+++ eval.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import argparse
+import os.path as osp
+import os
+from eval.evaluator import Eval_thread
+from eval.dataloader import EvalDataset
+
+from config import davis_path, fbms_path, mcl_path, uvsd_path, \
+    visal_path, vos_path, segtrack_path, davsod_path
+
+
+# from concurrent.futures import ThreadPoolExecutor
+
+if __name__ == "__main__":
+    dataset_names = ['DAVSOD']
+    snapshot = '20000'
+    ckpt_path = './ckpt'
+    exp_name = 'VideoSaliency_2020-10-31 17:25:22'
+    gt_root = {'davis': os.path.join(davis_path, 'GT'),
+               'DAVSOD': os.path.join(davsod_path, 'GT'),
+               'VOS': os.path.join(vos_path, 'GT')}
+    threads = []
+    for dataset in dataset_names:
+        save_path = os.path.join(ckpt_path, exp_name, '(%s) %s_%s' % (exp_name, dataset, snapshot))
+        loader = EvalDataset(osp.join(save_path), osp.join(gt_root[dataset]))
+        thread = Eval_thread(loader, 'ours', dataset, './', True)
+        threads.append(thread)
+    for thread in threads:
+        print(thread.run())
---
+++ eval/dataloader.py
@@ -0,0 +1,34 @@
+from torch.utils import data
+import torch
+import os
+from PIL import Image
+import numpy as np
+
+class EvalDataset(data.Dataset):
+    def __init__(self, img_root, label_root):
+        # lst_label_folders = sorted(os.listdir(label_root))
+        lst_pred_folders = sorted(os.listdir(img_root))
+        lst = []
+        for folder in lst_pred_folders:
+            imgs = os.listdir(os.path.join(img_root, folder))
+            imgs.sort()
+            for img in imgs:
+                lst.append(os.path.join(folder, img))
+
+        self.image_path = list(map(lambda x: os.path.join(img_root, x), lst))
+        self.label_path = list(map(lambda x: os.path.join(label_root, x), lst))
+        # print(self.image_path)
+        # print(self.image_path.sort())
+    def __getitem__(self, item):
+        pred = Image.open(self.image_path[item]).convert('L')
+        gt = Image.open(self.label_path[item]).convert('L')
+        if pred.size != gt.size:
+            pred = pred.resize(gt.size, Image.BILINEAR)
+        pred_np = np.array(pred)
+        pred_np = ((pred_np - pred_np.min()) / (pred_np.max() - pred_np.min()) * 255).astype(np.uint8)
+        pred = Image.fromarray(pred_np)
+
+        return pred, gt
+
+    def __len__(self):
+        return len(self.image_path)
