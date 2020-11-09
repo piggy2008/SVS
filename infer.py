9af996a1c3a957e7d4cc90faf50e774137b3c8fd
piggy2008 <810656271@qq.com>
2020-11-09 10:20:53 +0800

    test tmc+sum

--- infer.py
+++ infer.py
@@ -29,7 +29,7 @@ exp_name = 'VideoSaliency_2020-11-08 15:17:09'
 
 args = {
     'gnn': True,
-    'snapshot': '20000',  # your snapshot filename (exclude extension name)
+    'snapshot': '40000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
