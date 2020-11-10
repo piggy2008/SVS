42acc713552287a5bec58b960a5212426d61cbb2
piggy2008 <810656271@qq.com>
2020-11-10 12:55:38 +0800

    test 4input no gnn fuse change

--- module/TMC.py
+++ module/TMC.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from utils.utils_mine import visualize_vec
+# from utils.utils_mine import visualize_vec
 
 class TMC(nn.Module):
     def __init__(self):
