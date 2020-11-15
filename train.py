356e8cb61726bef222f4a2d9a22fc9fee8dd908c
piggy2008 <810656271@qq.com>
2020-11-15 22:31:04 +0800

    add distill loss

--- train.py
+++ train.py
@@ -240,12 +240,22 @@ def train_single(net, inputs, flows, labels, optimizer, curr_iter):
     loss8 = criterion_str(out3f, labels)
     loss9 = criterion_str(out4f, labels)
 
+    loss2_d = criterion(out2r, F.sigmoid(out2u))
+    loss3_d = criterion(out3r, F.sigmoid(out2u))
+    loss4_d = criterion(out4r, F.sigmoid(out2u))
+    loss5_d = criterion(out5r, F.sigmoid(out2u))
+
+    loss7_d = criterion_str(out2f, F.sigmoid(out2u))
+    loss8_d = criterion_str(out3f, F.sigmoid(out2u))
+    loss9_d = criterion_str(out4f, F.sigmoid(out2u))
+
     # loss10 = criterion_str(out1a, labels)
     # loss11 = criterion_str(out2a, labels)
 
     total_loss = (loss0 + loss1) / 2 + loss2 / 2 + loss3 / 4 + loss4 / 8 + loss5 / 16 \
                  + loss7 / 2 + loss8 / 4 + loss9 / 8
-
+    distill_loss = loss2_d / 2 + loss3_d / 4 + loss4_d / 8 + loss5_d / 16 + loss7_d / 2 + loss8_d / 4 + loss9 / 8
+    total_loss = total_loss + distill_loss
     total_loss.backward()
     optimizer.step()
 
