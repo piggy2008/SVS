010aa91e0041c19a3c66654c05f2d5aaa2a636e3
piggy2008 <810656271@qq.com>
2020-11-16 17:43:48 +0800

    test gnn embedding

--- infer.py
+++ infer.py
@@ -24,7 +24,7 @@ torch.cuda.set_device(0)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2020-11-16 02:54:16'
+exp_name = 'VideoSaliency_2020-11-16 07:00:27'
 
 
 args = {
