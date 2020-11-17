7a71ab1f9c7910b50d1744cf398b4a5970782a3f
piggy2008 <810656271@qq.com>
2020-11-17 12:40:33 +0800

    fix lr

--- infer.py
+++ infer.py
@@ -24,7 +24,7 @@ torch.cuda.set_device(0)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2020-11-16 02:02:39'
+exp_name = 'VideoSaliency_2020-11-17 02:02:39'
 
 
 args = {
