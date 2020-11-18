67f030c5b7664a1753e3afc38fa3d7f9e43d5b82
piggy2008 <810656271@qq.com>
2020-11-18 21:47:08 +0800

    test 3 decoder

--- infer.py
+++ infer.py
@@ -140,8 +140,8 @@ def main():
 
                     start = time.time()
 
-                    prediction2, prediction, _, _, _, _, _, _, _, _, _ = net(img_var, flow_var)
-                    prediction = torch.sigmoid(prediction)
+                    prediction2, prediction, _, _, _, _, _, _, _, _, prediction3 = net(img_var, flow_var)
+                    prediction = torch.sigmoid(prediction3)
 
                     end = time.time()
                     print('running time:', (end - start))
