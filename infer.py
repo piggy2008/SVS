ca45a1f68f0d93d046831737e299a2c0c09e595b
piggy2008 <810656271@qq.com>
2020-11-19 14:39:51 +0800

    bug

--- infer.py
+++ infer.py
@@ -29,7 +29,7 @@ exp_name = 'VideoSaliency_2020-11-19 03:19:39'
 
 args = {
     'gnn': True,
-    'snapshot': '5000',  # your snapshot filename (exclude extension name)
+    'snapshot': '10000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
@@ -141,7 +141,7 @@ def main():
                     start = time.time()
 
                     prediction2, prediction, _, _, _, _, _, _, _, prediction3 = net(img_var, flow_var)
-                    prediction = torch.sigmoid(prediction)
+                    prediction = torch.sigmoid(prediction3)
 
                     end = time.time()
                     print('running time:', (end - start))
