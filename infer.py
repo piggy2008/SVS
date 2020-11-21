8ec11c8a4c113b28736787618653dc08d67600fd
piggy2008 <810656271@qq.com>
2020-11-21 21:01:59 +0800

    add gate in decoder1,2,3

--- infer.py
+++ infer.py
@@ -29,7 +29,7 @@ exp_name = 'VideoSaliency_2020-11-21 11:10:33'
 
 args = {
     'gnn': True,
-    'snapshot': '4000',  # your snapshot filename (exclude extension name)
+    'snapshot': '6000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
