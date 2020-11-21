caf15535c65c2a7e03f8ff64cc9ea5af6b6b635b
piggy2008 <810656271@qq.com>
2020-11-21 10:34:06 +0800

    add gate in decoder2,3

--- infer.py
+++ infer.py
@@ -24,12 +24,12 @@ torch.cuda.set_device(0)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2020-11-20 10:53:04'
+exp_name = 'VideoSaliency_2020-11-20 13:35:34'
 
 
 args = {
     'gnn': True,
-    'snapshot': '5000',  # your snapshot filename (exclude extension name)
+    'snapshot': '10000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
