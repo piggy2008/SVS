5e90ea05d3f488691d6f0b882e027b2f258adc04
piggy2008 <810656271@qq.com>
2020-11-22 09:57:55 +0800

    add fc in mmtm + add kl in distall

--- infer.py
+++ infer.py
@@ -29,7 +29,7 @@ exp_name = 'VideoSaliency_2020-11-21 14:21:23'
 
 args = {
     'gnn': True,
-    'snapshot': '8000',  # your snapshot filename (exclude extension name)
+    'snapshot': '12000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
