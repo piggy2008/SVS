323f34a94bc037ef9d4b1499dc0b29ce116bc718
piggy2008 <810656271@qq.com>
2020-11-25 09:37:07 +0800

    add gcn in mmtm + add kl in distall+ mga distill

--- infer.py
+++ infer.py
@@ -24,7 +24,7 @@ torch.cuda.set_device(1)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2020-11-24 12:42:43'
+exp_name = 'VideoSaliency_2020-11-24 22:22:20'
 
 
 args = {
