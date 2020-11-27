81fdf62be3128c168aeedc6902e4c8650c554081
piggy2008 <810656271@qq.com>
2020-11-27 11:43:29 +0800

    add mmtm + self distill + traditional distill

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2020-11-26 15:27:35'
 
 args = {
     'gnn': True,
-    'snapshot': '16000',  # your snapshot filename (exclude extension name)
+    'snapshot': '8000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
