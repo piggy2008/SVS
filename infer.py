51890d4cc32302901e0299c8f8404b9d4687d13f
piggy2008 <810656271@qq.com>
2020-12-02 14:07:56 +0800

    dynamic edge in gnn

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2020-12-01 22:04:59'
 
 args = {
     'gnn': True,
-    'snapshot': '32000',  # your snapshot filename (exclude extension name)
+    'snapshot': '44000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
