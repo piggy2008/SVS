9625cbc84f8e82c8239fad701c20ccac0181e3a3
piggy2008 <810656271@qq.com>
2020-12-04 11:09:34 +0800

    fix gcn edge

--- module/GCN.py
+++ module/GCN.py
@@ -24,7 +24,7 @@ class GraphConvolution(nn.Module):
         self.adj_size = adj_size
 
         self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
-        self.adj = nn.Parameter(torch.FloatTensor(adj_size, adj_size))
+        # self.adj = nn.Parameter(torch.FloatTensor(adj_size, adj_size))
 
         if bias:
             self.bias = nn.Parameter(torch.FloatTensor(out_features))
@@ -39,12 +39,12 @@ class GraphConvolution(nn.Module):
         self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
-        self.adj.data.fill_(1)
+        # self.adj.data.fill_(1)
 
     def forward(self, input, adj):
         support = torch.matmul(input, self.weight)
-        adj = torch.sigmoid(self.adj)
-        adj = adj.repeat(input.size(0), 1, 1)
+        # adj = torch.sigmoid(self.adj)
+        # adj = adj.repeat(input.size(0), 1, 1)
         output_ = torch.bmm(adj, support)
         if self.bias is not None:
             output_ = output_ + self.bias
