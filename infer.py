5832f52b25fe2d94a521745f33f41dcdd0473c5a
piggy2008 <810656271@qq.com>
2020-12-05 10:41:14 +0800

    fix gcn edge

--- infer.py
+++ infer.py
@@ -18,7 +18,7 @@ from matplotlib import pyplot as plt
 torch.manual_seed(2020)
 
 # set which gpu to use
-device_id = 3
+device_id = 0
 torch.cuda.set_device(device_id)
 
 # the following two args specify the location of the file of trained model (pth extension)
@@ -49,10 +49,10 @@ to_pil = transforms.ToPILImage()
 # flow_root = os.path.join(davis_path, 'flow')
 # imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
 
-# to_test = {'FBMS': os.path.join(fbms_path, 'FBMS_Testset')}
-# gt_root = os.path.join(fbms_path, 'GT')
-# flow_root = os.path.join(fbms_path, 'FBMS_Testset_flownet2_image')
-# imgs_path = os.path.join(fbms_path, 'FBMS_test_single.txt')
+to_test = {'FBMS': os.path.join(fbms_path, 'FBMS_Testset')}
+gt_root = os.path.join(fbms_path, 'GT')
+flow_root = os.path.join(fbms_path, 'FBMS_Testset_flownet2_image')
+imgs_path = os.path.join(fbms_path, 'FBMS_test_single.txt')
 
 # to_test = {'SegTrackV2': os.path.join(segtrack_path, 'SegTrackV2_test')}
 # gt_root = os.path.join(segtrack_path, 'GT')
@@ -68,10 +68,10 @@ to_pil = transforms.ToPILImage()
 # flow_root = os.path.join(vos_path, 'flow')
 # imgs_path = os.path.join(vos_path, 'VOS_test_single.txt')
 
-to_test = {'DAVSOD': os.path.join(davsod_path, 'DAVSOD_test')}
-gt_root = os.path.join(davsod_path, 'GT')
-flow_root = os.path.join(davsod_path, 'flow')
-imgs_path = os.path.join(davsod_path, 'DAVSOD_test_single.txt')
+# to_test = {'DAVSOD': os.path.join(davsod_path, 'DAVSOD_test')}
+# gt_root = os.path.join(davsod_path, 'GT')
+# flow_root = os.path.join(davsod_path, 'flow')
+# imgs_path = os.path.join(davsod_path, 'DAVSOD_test_single.txt')
 
 # to_test = {'MCL': os.path.join(mcl_path, 'MCL_test')}
 # gt_root = os.path.join(mcl_path, 'GT')
