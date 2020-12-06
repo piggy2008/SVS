e02cf905c673c26a11b0b4acfde8c36b335b9e1d
piggy2008 <810656271@qq.com>
2020-12-06 16:50:36 +0800

    add iterative training and dut-tr for train

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2020-12-06 10:54:05'
 
 args = {
     'gnn': True,
-    'snapshot': '8000',  # your snapshot filename (exclude extension name)
+    'snapshot': '28000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
@@ -148,7 +148,7 @@ def main():
                     start = time.time()
 
                     prediction2, prediction, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, prediction3 = net(img_var, flow_var)
-                    prediction = torch.sigmoid(prediction)
+                    prediction = torch.sigmoid(prediction3)
 
                     end = time.time()
                     print('running time:', (end - start))
