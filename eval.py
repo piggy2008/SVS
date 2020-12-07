502b58dfd180f9eae559ce72a6b72591c0c74218
piggy2008 <810656271@qq.com>
2020-12-07 17:57:33 +0800

    add iterative training and dut-tr for train

--- eval.py
+++ eval.py
@@ -13,7 +13,7 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 # from concurrent.futures import ThreadPoolExecutor
 
 if __name__ == "__main__":
-    dataset_names = ['davis']
+    dataset_names = ['DAVSOD']
     snapshot = '72000'
     ckpt_path = './ckpt'
     exp_name = 'VideoSaliency_2020-12-06 19:46:37'
