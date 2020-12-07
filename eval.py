b788a4e20c2bc9bf07f416c22291398e8f591b32
piggy2008 <810656271@qq.com>
2020-12-07 14:46:24 +0800

    add iterative training and dut-tr for train

--- eval.py
+++ eval.py
@@ -13,11 +13,11 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 # from concurrent.futures import ThreadPoolExecutor
 
 if __name__ == "__main__":
-    dataset_names = ['DAVIS']
+    dataset_names = ['davis']
     snapshot = '72000'
     ckpt_path = './ckpt'
     exp_name = 'VideoSaliency_2020-12-06 19:46:37'
-    gt_root = {'DAVIS': os.path.join(davis_path, 'GT'),
+    gt_root = {'davis': os.path.join(davis_path, 'GT'),
                'DAVSOD': os.path.join(davsod_path, 'GT'),
                'VOS': os.path.join(vos_path, 'GT')}
     threads = []
