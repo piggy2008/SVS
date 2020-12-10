a0f96600cc55ec64231d12e3d1b225bc6931efbf
piggy2008 <810656271@qq.com>
2020-12-10 21:21:50 +0800

    add iterative training and dut-tr for train plus trainable adj

--- module/GCN.py
+++ module/GCN.py
@@ -39,11 +39,11 @@ class GraphConvolution(nn.Module):
         self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
-        # self.adj.data.fill_(1)
+        self.adj.data.fill_(1)
 
     def forward(self, input, adj):
         support = torch.matmul(input, self.weight)
-        adj = torch.softmax(self.adj)
+        adj = torch.sigmoid(self.adj)
         adj = adj.repeat(input.size(0), 1, 1)
         output_ = torch.bmm(adj, support)
         if self.bias is not None:
