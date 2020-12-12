c37b252c34796eefba634fe2dc15c19353625d8c
piggy2008 <810656271@qq.com>
2020-12-12 17:42:22 +0800

    add iterative training and dut-tr for train plus trainable adj

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2020-12-11 22:07:07'
 
 args = {
     'gnn': True,
-    'snapshot': '52000',  # your snapshot filename (exclude extension name)
+    'snapshot': '56000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
