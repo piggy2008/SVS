d4d2d04b1b8e7618e05f1942311c197c17827d19
piggy2008 <810656271@qq.com>
2020-12-15 14:35:07 +0800

    add saliency guide with gcn to refine feat from rgb and flow

--- infer.py
+++ infer.py
@@ -19,18 +19,18 @@ from matplotlib import pyplot as plt
 torch.manual_seed(2020)
 
 # set which gpu to use
-device_id = 1
+device_id = 0
 torch.cuda.set_device(device_id)
 
 # the following two args specify the location of the file of trained model (pth extension)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2020-12-14 11:46:04'
+exp_name = 'VideoSaliency_2020-12-15 11:13:33'
 
 args = {
     'gnn': True,
-    'snapshot': '80000',  # your snapshot filename (exclude extension name)
+    'snapshot': '12000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
