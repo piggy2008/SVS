a30470713234e204f02e3893e906d4b880075e22
piggy2008 <810656271@qq.com>
2020-12-16 10:01:23 +0800

    add saliency guide with gcn to refine feat from rgb and flow

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2020-12-15 11:13:33'
 
 args = {
     'gnn': True,
-    'snapshot': '68000',  # your snapshot filename (exclude extension name)
+    'snapshot': '72000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
