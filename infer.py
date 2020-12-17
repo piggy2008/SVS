d8fc628cbe26cb354fc93fca71bada958086e363
piggy2008 <810656271@qq.com>
2020-12-17 15:37:30 +0800

    add duts+dut_ts for single frame training

--- infer.py
+++ infer.py
@@ -26,11 +26,11 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2020-12-16 10:56:53'
+exp_name = 'VideoSaliency_2020-12-17 10:29:55'
 
 args = {
     'gnn': True,
-    'snapshot': '68000',  # your snapshot filename (exclude extension name)
+    'snapshot': '20000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
