cff3e7d84e6e05841c859a4f90c381251c93696e
piggy2008 <810656271@qq.com>
2020-12-18 11:12:56 +0800

    global refine with flow features

--- models/net_i.py
+++ models/net_i.py
@@ -438,7 +438,7 @@ class INet(nn.Module):
             out2h, out3h, out4h, out5v = self.se_many(feat_list, pred1)
             out2f, out3f, out4f = self.se_many_flow(feat_flow_list, pred1)
             out2h, out3h, out4h, out5v, out2f, out3f, out4f, pred2 = self.decoder2(out2h, out3h, out4h, out5v, out2f, out3f, out4f, pred1)
-            feat_list2 = [out2h, out3h, out4h, out5v]
+            feat_list2 = [out2h, out3h, out4h, out5v, out4f]
             out2h, out3h, out4h, out5v = self.se_many2(feat_list2, pred2)
             out2f = F.interpolate(out2f, size=out2f_scale, mode='bilinear')
             out3f = F.interpolate(out3f, size=out3f_scale, mode='bilinear')
