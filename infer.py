3c175ca66f12a98a5ffa5158527b945386a8297a
piggy2008 <810656271@qq.com>
2020-12-19 19:47:00 +0800

    fix and finetune model

--- infer.py
+++ infer.py
@@ -26,11 +26,11 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2020-12-18 15:35:32'
+exp_name = 'VideoSaliency_2020-12-19 15:41:53'
 
 args = {
     'gnn': True,
-    'snapshot': '100000',  # your snapshot filename (exclude extension name)
+    'snapshot': '1000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
