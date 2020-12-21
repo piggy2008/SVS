650a643388e5c016f473c18b21084a850f6f42c4
piggy2008 <810656271@qq.com>
2020-12-21 09:58:57 +0800

    finetune with global

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2020-12-20 21:14:02'
 
 args = {
     'gnn': True,
-    'snapshot': '6000',  # your snapshot filename (exclude extension name)
+    'snapshot': '7000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
