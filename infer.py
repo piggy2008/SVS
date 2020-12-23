64820ce4af49eaab99e3ab7f88fa87e2b676db6f
piggy2008 <810656271@qq.com>
2020-12-23 21:23:28 +0800

    add glocal refine like local

--- infer.py
+++ infer.py
@@ -26,11 +26,11 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2020-12-22 22:26:35'
+exp_name = 'VideoSaliency_2020-12-23 11:50:15'
 
 args = {
     'gnn': True,
-    'snapshot': '76000',  # your snapshot filename (exclude extension name)
+    'snapshot': '40000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
