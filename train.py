6e235e8e7619d37613f3eb14352260970717d9e3
piggy <810656271@qq.com>
2020-12-24 01:38:44 +0800

    update

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ args = {
     'L2': False,
     'KL': True,
     'structure': True,
-    'iter_num': 80000,
+    'iter_num': 100000,
     'iter_save': 4000,
     'iter_start_seq': 0,
     'train_batch_size': 6,
