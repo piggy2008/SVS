87c9c85e60bec0e98e34bc3f9e9713889eed9dd7
piggy2008 <810656271@qq.com>
2020-12-25 10:28:33 +0800

    add glocal refine like local

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2020-12-24 18:54:17'
 
 args = {
     'gnn': True,
-    'snapshot': '88000',  # your snapshot filename (exclude extension name)
+    'snapshot': '32000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
