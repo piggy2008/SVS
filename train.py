33debc43c8f1c0dcf78a8d5c03688aa551547886
piggy2008 <810656271@qq.com>
2020-12-31 20:01:15 +0800

    2 decoders plus local and global and adjust distill weights 0.1 0.3

--- train.py
+++ train.py
@@ -71,7 +71,8 @@ args = {
     # 'train_loader': 'video_sequence'
     'image_size': 430,
     'crop_size': 380,
-    'self_distill': 0.1
+    'self_distill': 0.1,
+    'teacher_distill': 0.3
 }
 
 imgs_file = os.path.join(datasets_root, args['imgs_file'])
