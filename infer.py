7a51943716bcb940a8275f45ee64278e77693559
piggy2008 <810656271@qq.com>
2021-01-01 18:11:18 +0800

    2 decoders plus local and global and adjust distill weights 0.1 0.3

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2020-12-31 20:02:07'
 
 args = {
     'gnn': True,
-    'snapshot': '68000',  # your snapshot filename (exclude extension name)
+    'snapshot': '72000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
