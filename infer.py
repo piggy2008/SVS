f47bb3b1f3070c49e8fa3f949e6a2ab466fe4584
piggy2008 <810656271@qq.com>
2021-01-04 09:59:32 +0800

    2 decoders plus local and global and adjust distill weights 0.1 0.5

--- infer.py
+++ infer.py
@@ -26,11 +26,11 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2021-01-02 20:34:38'
+exp_name = 'VideoSaliency_2021-01-03 19:49:18'
 
 args = {
     'gnn': True,
-    'snapshot': '100000',  # your snapshot filename (exclude extension name)
+    'snapshot': '8000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
