15cf1b07da74812bd01e34f0b2721f7de1093086
piggy2008 <810656271@qq.com>
2021-01-11 22:05:26 +0800

    ablation study, without GRM and LFM

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2021-01-11 10:49:12'
 
 args = {
     'gnn': True,
-    'snapshot': '52000',  # your snapshot filename (exclude extension name)
+    'snapshot': '48000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
@@ -45,10 +45,10 @@ to_pil = transforms.ToPILImage()
 # to_test = {'ecssd': ecssd_path, 'hkuis': hkuis_path, 'pascal': pascals_path, 'sod': sod_path, 'dutomron': dutomron_path}
 # to_test = {'ecssd': ecssd_path}
 
-# to_test = {'davis': os.path.join(davis_path, 'davis_test2')}
-# gt_root = os.path.join(davis_path, 'GT')
-# flow_root = os.path.join(davis_path, 'flow')
-# imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
+to_test = {'davis': os.path.join(davis_path, 'davis_test2')}
+gt_root = os.path.join(davis_path, 'GT')
+flow_root = os.path.join(davis_path, 'flow')
+imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
 
 # to_test = {'FBMS': os.path.join(fbms_path, 'FBMS_Testset')}
 # gt_root = os.path.join(fbms_path, 'GT')
@@ -59,10 +59,10 @@ to_pil = transforms.ToPILImage()
 # gt_root = os.path.join(segtrack_path, 'GT')
 # imgs_path = os.path.join(segtrack_path, 'SegTrackV2_test_single.txt')
 
-to_test = {'ViSal': os.path.join(visal_path, 'ViSal_test')}
-gt_root = os.path.join(visal_path, 'GT')
-flow_root = os.path.join(visal_path, 'flow')
-imgs_path = os.path.join(visal_path, 'ViSal_test_single.txt')
+# to_test = {'ViSal': os.path.join(visal_path, 'ViSal_test')}
+# gt_root = os.path.join(visal_path, 'GT')
+# flow_root = os.path.join(visal_path, 'flow')
+# imgs_path = os.path.join(visal_path, 'ViSal_test_single.txt')
 
 # to_test = {'VOS': os.path.join(vos_path, 'VOS_test')}
 # gt_root = os.path.join(vos_path, 'GT')
