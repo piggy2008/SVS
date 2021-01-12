722caba476592beead9e5a0fb85f23bd0b8a5563
piggy2008 <810656271@qq.com>
2021-01-12 21:00:54 +0800

    ablation study, without GRM and LFM and structure loss

--- infer.py
+++ infer.py
@@ -26,11 +26,11 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2021-01-11 10:49:12'
+exp_name = 'VideoSaliency_2021-01-11 10:17:03'
 
 args = {
     'gnn': True,
-    'snapshot': '100000',  # your snapshot filename (exclude extension name)
+    'snapshot': '32000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
