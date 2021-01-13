f2d00bfae80db07250326cd3cc2d7932f071a7c6
piggy2008 <810656271@qq.com>
2021-01-13 10:12:18 +0800

    ablation study, without gcn

--- eval.py
+++ eval.py
@@ -14,9 +14,9 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 
 if __name__ == "__main__":
     dataset_names = ['ViSal']
-    snapshot = '12000'
+    snapshot = '44000'
     ckpt_path = '/home/ty/code/SVS/ckpt'
-    exp_name = 'VideoSaliency_2021-01-12 10:17:03'
+    exp_name = 'VideoSaliency_2021-01-12 21:56:03'
     gt_root = {'davis': os.path.join(davis_path, 'GT'),
                'DAVSOD': os.path.join(davsod_path, 'GT'),
                'VOS': os.path.join(vos_path, 'GT'),
