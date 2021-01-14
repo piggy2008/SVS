a33982bc19865f9f2f26ea8f83ce6e4ca02ffeb0
piggy2008 <810656271@qq.com>
2021-01-14 20:43:08 +0800

    finetune p3

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ args = {
     'iter_num': 100000,
     'iter_save': 4000,
     'iter_start_seq': 0,
-    'train_batch_size': 6,
+    'train_batch_size': 4,
     'last_iter': 0,
     'lr': 1e-3,
     'lr_decay': 0.9,
