256cf123fecc0b503d3b9f91587108af87c44a66
piggy2008 <810656271@qq.com>
2021-01-14 13:56:27 +0800

    test cosine adj

--- infer.py
+++ infer.py
@@ -19,7 +19,7 @@ from matplotlib import pyplot as plt
 torch.manual_seed(2020)
 
 # set which gpu to use
-device_id = 1
+device_id = 0
 torch.cuda.set_device(device_id)
 
 # the following two args specify the location of the file of trained model (pth extension)
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2021-01-13 22:54:10'
 
 args = {
     'gnn': True,
-    'snapshot': '52000',  # your snapshot filename (exclude extension name)
+    'snapshot': '56000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
