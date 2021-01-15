b9f859b5fbe461cc2fdb1a3fa582cef0b1cbfc3d
piggy2008 <810656271@qq.com>
2021-01-15 19:34:28 +0800

    test triplet

--- models/net_i.py
+++ models/net_i.py
@@ -265,8 +265,8 @@ class SFM2(nn.Module):
         out2l = self.conv2l(out1l)
         out1f = self.conv1f(flow)
         out2f = self.conv2f(out1f)
-        # fuse = out2h * out2l * out2f
-        fuse = self.se_triplet(out2h, out2l, out2f)
+        fuse = out2h * out2l * out2f
+        # fuse = self.se_triplet(out2h, out2l, out2f)
         out3h = self.conv3h(fuse) + out1h
         out4h = self.conv4h(out3h)
         out3l = self.conv3l(fuse) + out1l
