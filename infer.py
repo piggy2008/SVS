707eef3ab22230da83babecd64f98648761cd2fa
piggy2008 <810656271@qq.com>
2021-01-16 20:32:33 +0800

    test p3

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2021-01-15 21:59:10'
 
 args = {
     'gnn': True,
-    'snapshot': '76000',  # your snapshot filename (exclude extension name)
+    'snapshot': '80000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
