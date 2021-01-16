e6783555ca1d7bfebf20256591e0a5a2012508a3
piggy2008 <810656271@qq.com>
2021-01-16 17:56:59 +0800

    test triplet

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2021-01-15 19:56:13'
 
 args = {
     'gnn': True,
-    'snapshot': '64000',  # your snapshot filename (exclude extension name)
+    'snapshot': '68000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
