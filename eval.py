b78ff7baca2188a9b96a694d9b5a874bbfc5a4f5
piggy2008 <810656271@qq.com>
2021-01-16 20:53:51 +0800

    test p3

--- eval.py
+++ eval.py
@@ -14,9 +14,9 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 
 if __name__ == "__main__":
     dataset_names = ['ViSal']
-    snapshot = '40000'
+    snapshot = '52000'
     ckpt_path = '/home/ty/code/SVS/ckpt'
-    exp_name = 'VideoSaliency_2021-01-13 14:45:27'
+    exp_name = 'VideoSaliency_2021-01-14 17:50:56'
     gt_root = {'davis': os.path.join(davis_path, 'GT'),
                'DAVSOD': os.path.join(davsod_path, 'GT'),
                'VOS': os.path.join(vos_path, 'GT'),
