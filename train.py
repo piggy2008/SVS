40ec7b9e2f1448dae63e35f7e16af299021b439e
piggy2008 <810656271@qq.com>
2021-01-19 17:00:32 +0800

    test triplet

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ import numpy as np
 
 cudnn.benchmark = True
 
-device_id = 1
+device_id = 0
 
 torch.manual_seed(2021)
 torch.cuda.manual_seed(2021)
@@ -54,13 +54,13 @@ args = {
     'iter_start_seq': 0,
     'train_batch_size': 7,
     'last_iter': 0,
-    'lr': 5 * 1e-3,
+    'lr': 4 * 1e-3,
     'lr_decay': 0.9,
     'weight_decay': 5e-4,
     'momentum': 0.925,
     'snapshot': '',
-    # 'pretrain': os.path.join(ckpt_path, 'VideoSaliency_2021-01-03 19:49:18', '56000.pth'),
-    'pretrain': '',
+    'pretrain': os.path.join(ckpt_path, 'VideoSaliency_2021-01-03 19:49:18', '20000.pth'),
+    # 'pretrain': '',
     'mga_model_path': 'pre-trained/MGA_trained.pth',
     # 'imgs_file': 'Pre-train/pretrain_all_seq_DUT_DAFB2_DAVSOD.txt',
     'imgs_file': 'Pre-train/pretrain_all_seq_DAFB2_DAVSOD_flow.txt',
