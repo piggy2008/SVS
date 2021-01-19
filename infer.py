b2841df5263bb3e1585ff058f22f7dc3c2a62917
piggy2008 <810656271@qq.com>
2021-01-19 09:16:24 +0800

    test triplet

--- infer.py
+++ infer.py
@@ -26,11 +26,11 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2021-01-18 20:36:50'
+exp_name = 'VideoSaliency_2021-01-18 11:03:40'
 
 args = {
     'gnn': True,
-    'snapshot': '2000',  # your snapshot filename (exclude extension name)
+    'snapshot': '44000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
