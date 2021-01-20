e8d3d1e3a56c391ac2d0311e7f48a3b42f894111
piggy2008 <810656271@qq.com>
2021-01-20 15:16:54 +0800

    test triplet and p3

--- infer.py
+++ infer.py
@@ -26,11 +26,11 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2021-01-19 22:51:04'
+exp_name = 'VideoSaliency_2021-01-10 10:33:04'
 
 args = {
     'gnn': True,
-    'snapshot': '24000',  # your snapshot filename (exclude extension name)
+    'snapshot': '20000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
@@ -147,8 +147,8 @@ def main():
 
                     start = time.time()
 
-                    prediction2, prediction, _, _, _, _, _, _, _, _, _, _, _, _, _, _ = net(img_var, flow_var)
-                    prediction = torch.sigmoid(prediction)
+                    prediction2, prediction, prediction3, _, _, _, _, _, _, _, _, _, _, _, _, _, _ = net(img_var, flow_var)
+                    prediction = torch.sigmoid(prediction3)
 
                     end = time.time()
                     print('running time:', (end - start))
