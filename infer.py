e65a72656101aac442a279d85080157e5063acdf
piggy2008 <810656271@qq.com>
2021-02-21 10:06:37 +0800

    test p3 direct adj

--- infer.py
+++ infer.py
@@ -26,11 +26,11 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2021-01-14 17:50:56'
+exp_name = 'VideoSaliency_2021-02-20 11:20:51'
 
 args = {
     'gnn': True,
-    'snapshot': '52000',  # your snapshot filename (exclude extension name)
+    'snapshot': '36000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
