3d9145d9703af4da946ab448d974af397c95f3cd
piggy2008 <810656271@qq.com>
2021-02-27 12:02:42 +0800

    test p3 consine adj

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ args = {
     'lr': 5 * 1e-3,
     'lr_decay': 0.9,
     'weight_decay': 5e-4,
-    'momentum': 0.93,
+    'momentum': 0.95,
     'snapshot': '',
     # 'pretrain': os.path.join(ckpt_path, 'VideoSaliency_2021-01-03 19:49:18', '44000.pth'),
     'pretrain': '',
@@ -72,7 +72,7 @@ args = {
     'image_size': 430,
     'crop_size': 380,
     'self_distill': 0.1,
-    'teacher_distill': 0.5
+    'teacher_distill': 0.4
 }
 
 imgs_file = os.path.join(datasets_root, args['imgs_file'])
