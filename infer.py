5e60c86473b0c838645bde9879dd86896d1167d8
piggy2008 <810656271@qq.com>
2021-02-28 17:17:02 +0800

    test p3 consine adj

--- infer.py
+++ infer.py
@@ -19,18 +19,18 @@ from matplotlib import pyplot as plt
 torch.manual_seed(2020)
 
 # set which gpu to use
-device_id = 0
+device_id = 2
 torch.cuda.set_device(device_id)
 
 # the following two args specify the location of the file of trained model (pth extension)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2021-02-27 12:08:26'
+exp_name = 'VideoSaliency_2021-02-28 10:46:00'
 
 args = {
     'gnn': True,
-    'snapshot': '40000',  # your snapshot filename (exclude extension name)
+    'snapshot': '20000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
