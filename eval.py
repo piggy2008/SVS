4211d674d606e979c1d6a162e6b8c073515aefe0
piggy2008 <810656271@qq.com>
2021-03-02 20:58:33 +0800

    test p3 consine adj

--- eval.py
+++ eval.py
@@ -13,7 +13,7 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 # from concurrent.futures import ThreadPoolExecutor
 
 if __name__ == "__main__":
-    dataset_names = ['DAVSOD']
+    dataset_names = ['davis']
     snapshot = '68000'
     ckpt_path = '/home/ty/code/SVS/ckpt'
     exp_name = 'VideoSaliency_2021-03-01 22:16:11'
