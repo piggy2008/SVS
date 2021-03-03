ed92f35d32225703b13baef9cab55282dd3283bb
piggy2008 <810656271@qq.com>
2021-03-03 16:04:56 +0800

    test p3 consine adj

--- eval.py
+++ eval.py
@@ -14,7 +14,7 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 
 if __name__ == "__main__":
     dataset_names = ['davis']
-    snapshot = '80000'
+    snapshot = '168000'
     ckpt_path = '/home/ty/code/SVS/ckpt'
     exp_name = 'VideoSaliency_2021-03-01 22:16:11'
     gt_root = {'davis': os.path.join(davis_path, 'GT'),
