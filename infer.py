5b92cd20ec972dba59d6a29e93ead2d6fbbb7cb3
piggy2008 <810656271@qq.com>
2021-03-05 21:00:31 +0800

    test p3 consine adj

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2021-03-05 11:05:09'
 
 args = {
     'gnn': True,
-    'snapshot': '36000',  # your snapshot filename (exclude extension name)
+    'snapshot': '40000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
