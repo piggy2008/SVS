243e04d868e9b4e5fcdbc1b092b38c167ef4958c
piggy2008 <810656271@qq.com>
2021-03-21 15:23:18 +0800

    test p3 and glocal all

--- infer.py
+++ infer.py
@@ -30,7 +30,7 @@ exp_name = 'VideoSaliency_2021-03-20 12:29:02'
 
 args = {
     'gnn': True,
-    'snapshot': '180000',  # your snapshot filename (exclude extension name)
+    'snapshot': '184000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380),
