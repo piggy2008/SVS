049b597e02eca709f202c91595cabcc0c317977d
piggy2008 <810656271@qq.com>
2021-03-26 21:09:41 +0800

    test res101

--- train.py
+++ train.py
@@ -199,7 +199,7 @@ def main():
         optimizer.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, args['snapshot'] + '_optim.pth')))
         optimizer.param_groups[0]['lr'] = 0.5 * args['lr']
         optimizer.param_groups[1]['lr'] = args['lr']
-        optimizer.param_groups[2]['lr'] = 0.5 * args['lr']
+        optimizer.param_groups[2]['lr'] = args['lr']
 
     net = load_part_of_model(net, 'pre-trained/SNet101.pth', device_id=device_id)
     if len(args['pretrain']) > 0:
@@ -225,7 +225,7 @@ def train(net, optimizer, teacher=None):
                                                                   ) ** args['lr_decay']
             optimizer.param_groups[1]['lr'] = args['lr'] * (1 - float(curr_iter) / args['iter_num']
                                                             ) ** args['lr_decay']
-            optimizer.param_groups[2]['lr'] = 0.1 * args['lr'] * (1 - float(curr_iter) / args['iter_num']
+            optimizer.param_groups[2]['lr'] = args['lr'] * (1 - float(curr_iter) / args['iter_num']
                                                                   ) ** args['lr_decay']
             #
             # optimizer.param_groups[3]['lr'] = 0.1 * args['lr'] * (1 - float(curr_iter) / args['iter_num']
