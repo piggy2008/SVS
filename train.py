b5f7b6e206cf00ca106c66d8b76dc0ebd538b89b
piggy2008 <810656271@qq.com>
2021-03-26 17:58:05 +0800

    test res101

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ args = {
     'iter_num': 200000,
     'iter_save': 4000,
     'iter_start_seq': 0,
-    'train_batch_size': 4,
+    'train_batch_size': 5,
     'last_iter': 0,
     'lr': 5 * 1e-3,
     'lr_decay': 0.9,
