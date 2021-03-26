e09d36a783d9c552274f6093a8872bae35210770
piggy2008 <810656271@qq.com>
2021-03-26 16:35:09 +0800

    test res101

--- models/net_i101.py
+++ models/net_i101.py
@@ -67,7 +67,7 @@ class ResNet(nn.Module):
         self.bn1      = nn.BatchNorm2d(64)
         self.layer1   = self.make_layer( 64, 3, stride=1, dilation=1)
         self.layer2   = self.make_layer(128, 4, stride=2, dilation=1)
-        self.layer3   = self.make_layer(256, 6, stride=2, dilation=1)
+        self.layer3   = self.make_layer(256, 23, stride=2, dilation=1)
         self.layer4   = self.make_layer(512, 3, stride=2, dilation=1)
 
     def make_layer(self, planes, blocks, stride, dilation):
@@ -88,7 +88,7 @@ class ResNet(nn.Module):
         return out2, out3, out4, out5
 
     def initialize(self):
-        self.load_state_dict(torch.load('pre-trained/resnet50-19c8e357.pth'), strict=False)
+        self.load_state_dict(torch.load('pre-trained/resnet101-5d3b4d8f.pth'), strict=False)
 
 class GFM2(nn.Module):
     def __init__(self, GNN=False):
@@ -282,44 +282,6 @@ class SFM2(nn.Module):
     def initialize(self):
         weight_init(self)
 
-class CFM(nn.Module):
-    def __init__(self):
-        super(CFM, self).__init__()
-        self.conv1h = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn1h   = nn.BatchNorm2d(64)
-        self.conv2h = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn2h   = nn.BatchNorm2d(64)
-        self.conv3h = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn3h   = nn.BatchNorm2d(64)
-        self.conv4h = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn4h   = nn.BatchNorm2d(64)
-
-        self.conv1v = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn1v   = nn.BatchNorm2d(64)
-        self.conv2v = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn2v   = nn.BatchNorm2d(64)
-        self.conv3v = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn3v   = nn.BatchNorm2d(64)
-        self.conv4v = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn4v   = nn.BatchNorm2d(64)
-
-    def forward(self, left, down):
-        if down.size()[2:] != left.size()[2:]:
-            down = F.interpolate(down, size=left.size()[2:], mode='bilinear')
-        out1h = F.relu(self.bn1h(self.conv1h(left )), inplace=True)
-        out2h = F.relu(self.bn2h(self.conv2h(out1h)), inplace=True)
-        out1v = F.relu(self.bn1v(self.conv1v(down )), inplace=True)
-        out2v = F.relu(self.bn2v(self.conv2v(out1v)), inplace=True)
-        fuse  = out2h*out2v
-        out3h = F.relu(self.bn3h(self.conv3h(fuse )), inplace=True)+out1h
-        out4h = F.relu(self.bn4h(self.conv4h(out3h)), inplace=True)
-        out3v = F.relu(self.bn3v(self.conv3v(fuse )), inplace=True)+out1v
-        out4v = F.relu(self.bn4v(self.conv4v(out3v)), inplace=True)
-        return out4h, out4v
-
-    def initialize(self):
-        weight_init(self)
-
 class Decoder_flow(nn.Module):
     def __init__(self):
         super(Decoder_flow, self).__init__()
@@ -396,32 +358,6 @@ class Decoder_flow2(nn.Module):
             return out2h, out3h, out4h, out5v, out2b, out3b, out4b, pred
         else:
             return out2h, out3h, out4h, out5v, pred
-
-    def initialize(self):
-        weight_init(self)
-
-class Decoder(nn.Module):
-    def __init__(self):
-        super(Decoder, self).__init__()
-        self.cfm45  = CFM()
-        self.cfm34  = CFM()
-        self.cfm23  = CFM()
-
-    def forward(self, out2h, out3h, out4h, out5v, fback=None):
-        if fback is not None:
-            refine5      = F.interpolate(fback, size=out5v.size()[2:], mode='bilinear')
-            refine4      = F.interpolate(fback, size=out4h.size()[2:], mode='bilinear')
-            refine3      = F.interpolate(fback, size=out3h.size()[2:], mode='bilinear')
-            refine2      = F.interpolate(fback, size=out2h.size()[2:], mode='bilinear')
-            out5v        = out5v+refine5
-            out4h, out4v = self.cfm45(out4h+refine4, out5v)
-            out3h, out3v = self.cfm34(out3h+refine3, out4v)
-            out2h, pred  = self.cfm23(out2h+refine2, out3v)
-        else:
-            out4h, out4v = self.cfm45(out4h, out5v)
-            out3h, out3v = self.cfm34(out3h, out4v)
-            out2h, pred  = self.cfm23(out2h, out3v)
-        return out2h, out3h, out4h, out5v, pred
 
     def initialize(self):
         weight_init(self)
