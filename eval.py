f0354ad1d06c3ddc0d96478eab170d3d1d571efb
piggy2008 <810656271@qq.com>
2021-03-27 23:35:05 +0800

    test res101

--- eval.py
+++ eval.py
@@ -13,10 +13,10 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 # from concurrent.futures import ThreadPoolExecutor
 
 if __name__ == "__main__":
-    dataset_names = ['DAVSOD']
-    snapshot = '200000'
+    dataset_names = ['davis']
+    snapshot = '156000'
     ckpt_path = '/home/amax/code/SVS/ckpt'
-    exp_name = 'VideoSaliency_2021-03-22 21:07:37'
+    exp_name = 'VideoSaliency_2021-03-26 21:09:58'
     gt_root = {'davis': os.path.join(davis_path, 'GT'),
                'DAVSOD': os.path.join(davsod_path, 'GT'),
                'VOS': os.path.join(vos_path, 'GT'),
