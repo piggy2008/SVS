f44966f0af0571e003e376e1a816547ba43f196d
piggy2008 <810656271@qq.com>
2021-04-06 09:47:56 +0800

    test resnet101 p3

--- infer.py
+++ infer.py
@@ -27,7 +27,7 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
 
-exp_name = 'VideoSaliency_2021-04-04 15:30:05'
+exp_name = 'VideoSaliency_2021-04-03 12:03:06'
 
 args = {
     'gnn': True,
