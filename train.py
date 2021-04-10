58ea0d1f40b0c2f8f212ebb26be32cef721053d5
piggy2008 <810656271@qq.com>
2021-04-10 16:24:59 +0800

    test res50 p3

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ import numpy as np
 cudnn.benchmark = True
 
 device_id = 0
-device_id2 = 1
+device_id2 = 0
 
 torch.manual_seed(2021)
 torch.cuda.manual_seed(2021)
@@ -54,9 +54,9 @@ args = {
     'iter_num': 200000,
     'iter_save': 4000,
     'iter_start_seq': 0,
-    'train_batch_size': 10,
+    'train_batch_size': 5,
     'last_iter': 0,
-    'lr': 6.5 * 1e-3,
+    'lr': 5 * 1e-3,
     'lr_decay': 0.9,
     'weight_decay': 5e-4,
     'momentum': 0.925,
@@ -166,7 +166,7 @@ def main():
         teacher.eval()
         teacher.cuda(device_id2)
 
-    net = INet101(cfg=None, GNN=args['gnn']).cuda(device_id).train()
+    net = INet(cfg=None, GNN=args['gnn']).cuda(device_id).train()
     bkbone, flow_modules, remains = [], [], []
     for name, param in net.named_parameters():
         if 'bkbone' in name:
@@ -201,7 +201,7 @@ def main():
         optimizer.param_groups[1]['lr'] = args['lr']
         optimizer.param_groups[2]['lr'] = args['lr']
 
-    net = load_part_of_model(net, 'pre-trained/SNet101.pth', device_id=device_id)
+    net = load_part_of_model(net, 'pre-trained/SNet.pth', device_id=device_id)
     if len(args['pretrain']) > 0:
         print('pretrain model from ' + args['pretrain'])
         net = load_part_of_model(net, args['pretrain'], device_id=device_id)
