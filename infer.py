524790ab3386ffa995d3a7c24536ee1b7add50e3
piggy2008 <810656271@qq.com>
2021-04-10 09:59:45 +0800

    test res50 p3

--- infer.py
+++ infer.py
@@ -20,7 +20,7 @@ from matplotlib import pyplot as plt
 torch.manual_seed(2020)
 
 # set which gpu to use
-device_id = 0
+device_id = 1
 torch.cuda.set_device(device_id)
 
 # the following two args specify the location of the file of trained model (pth extension)
@@ -31,7 +31,7 @@ exp_name = 'VideoSaliency_2021-04-08 20:35:05'
 
 args = {
     'gnn': True,
-    'snapshot': '104000',  # your snapshot filename (exclude extension name)
+    'snapshot': '116000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380),
