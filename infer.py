6776ae5c3c71be417db6185312fd712dff2117c1
piggy2008 <810656271@qq.com>
2021-04-13 14:43:07 +0800

    test res101 p3

--- infer.py
+++ infer.py
@@ -31,7 +31,7 @@ exp_name = 'VideoSaliency_2021-04-10 12:52:52'
 
 args = {
     'gnn': True,
-    'snapshot': '196000',  # your snapshot filename (exclude extension name)
+    'snapshot': '72000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380),
@@ -47,10 +47,10 @@ to_pil = transforms.ToPILImage()
 # to_test = {'ecssd': ecssd_path, 'hkuis': hkuis_path, 'pascal': pascals_path, 'sod': sod_path, 'dutomron': dutomron_path}
 # to_test = {'ecssd': ecssd_path}
 
-to_test = {'davis': os.path.join(davis_path, 'davis_test2')}
-gt_root = os.path.join(davis_path, 'GT')
-flow_root = os.path.join(davis_path, 'flow')
-imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
+# to_test = {'davis': os.path.join(davis_path, 'davis_test2')}
+# gt_root = os.path.join(davis_path, 'GT')
+# flow_root = os.path.join(davis_path, 'flow')
+# imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
 
 # to_test = {'FBMS': os.path.join(fbms_path, 'FBMS_Testset')}
 # gt_root = os.path.join(fbms_path, 'GT')
@@ -71,10 +71,10 @@ imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
 # flow_root = os.path.join(vos_path, 'flow')
 # imgs_path = os.path.join(vos_path, 'VOS_test_single.txt')
 
-# to_test = {'DAVSOD': os.path.join(davsod_path, 'DAVSOD_test')}
-# gt_root = os.path.join(davsod_path, 'GT')
-# flow_root = os.path.join(davsod_path, 'flow')
-# imgs_path = os.path.join(davsod_path, 'DAVSOD_test_single.txt')
+to_test = {'DAVSOD': os.path.join(davsod_path, 'DAVSOD_test')}
+gt_root = os.path.join(davsod_path, 'GT')
+flow_root = os.path.join(davsod_path, 'flow')
+imgs_path = os.path.join(davsod_path, 'DAVSOD_test_single.txt')
 
 # to_test = {'MCL': os.path.join(mcl_path, 'MCL_test')}
 # gt_root = os.path.join(mcl_path, 'GT')
