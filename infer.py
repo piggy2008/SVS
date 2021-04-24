f136a7fb86ad09e09d22dfe0530ce9dc3ed684f3
piggy2008 <810656271@qq.com>
2021-04-24 20:17:33 +0800

    test res50 p3

--- infer.py
+++ infer.py
@@ -27,7 +27,7 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = saving_path
 
-exp_name = 'VideoSaliency_2021-04-22 16:20:01'
+exp_name = 'VideoSaliency_2021-04-23 20:08:46'
 
 args = {
     'gnn': True,
@@ -35,7 +35,7 @@ args = {
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380),
-    'start': 0
+    'start': 36000
 }
 
 img_transform = transforms.Compose([
