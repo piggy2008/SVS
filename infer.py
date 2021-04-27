7812b466c23da77272e5b89d7a30fc7cba7eaa48
piggy2008 <810656271@qq.com>
2021-04-27 15:03:26 +0800

    test res50 p3

--- infer.py
+++ infer.py
@@ -35,7 +35,7 @@ args = {
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380),
-    'start': 36000
+    'start': 60000
 }
 
 img_transform = transforms.Compose([
