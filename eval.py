3a7258e7cd961ce3bbe0b479cfde05c867e3f626
piggy2008 <810656271@qq.com>
2021-04-27 16:34:52 +0800

    test res50 p3

--- eval.py
+++ eval.py
@@ -14,7 +14,7 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 
 if __name__ == "__main__":
     dataset_names = ['ViSal']
-    snapshot = '156000'
+    snapshot = '164000'
     ckpt_path = '/home/amax/code/SVS/ckpt'
     exp_name = 'VideoSaliency_2021-04-26 13:13:52'
     gt_root = {'davis': os.path.join(davis_path, 'GT'),
