167e9fecb2d07b8b806d3cd561e375fed66b7bf2
piggy2008 <810656271@qq.com>
2021-04-28 21:57:12 +0800

    test res50 p3

--- eval.py
+++ eval.py
@@ -13,8 +13,8 @@ from config import davis_path, fbms_path, mcl_path, uvsd_path, \
 # from concurrent.futures import ThreadPoolExecutor
 
 if __name__ == "__main__":
-    dataset_names = ['davis']
-    snapshot = '196000'
+    dataset_names = ['DAVSOD']
+    snapshot = '184000'
     ckpt_path = '/home/amax/code/SVS/ckpt'
     exp_name = 'VideoSaliency_2021-04-27 16:53:42'
     gt_root = {'davis': os.path.join(davis_path, 'GT'),
